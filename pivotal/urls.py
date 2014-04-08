72f50e1a457af4a73aab3e5d7e610441aa7910a0
James Sullivan <jsullivan@cambridgecloudpartners.com>
2014-04-08 16:12:01 -0400

    show tasks under story

--- pivotal/urls.py
+++ pivotal/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^stories/(?P<project_id>\d+)/(?P<tag_filter>\S+)/(?P<hash_key>\S+)/$', 'pivotal.views.stories', name='pivotal_stories'),
+    url(r'^(?P<project_id>\d+)/stories/(?P<tag_filter>\w+)/(?P<hash_key>\w+)/$', 'pivotal.views.stories', name='pivotal_stories'),
+    url(r'^(?P<project_id>\d+)/stories/(?P<tag_filter>\w+)/(?P<hash_key>\w+)/(?P<story_id>\d+)/tasks/$', 'pivotal.views.tasks', name='pivotal_tasks'),
 )
--- pivotal/views.py
+++ pivotal/views.py
@@ -13,20 +13,43 @@ def _verify_hash(project_id, label, hash_key):
     key = "|".join((project_id, label, SECRET_KEY))
     true_hash = hashlib.sha256(key).hexdigest()
 
+    print true_hash, hash_key
+
     if true_hash != hash_key:
         raise Http404('Project not found')
 
 
-def _get_stories(project_id, label):
-    stories_url = "https://www.pivotaltracker.com" \
-                  "/services/v5/projects/%s/stories" \
-                  "?with_label=%s" % (project_id, label)
-    res = requests.get(stories_url,
-                       headers={"X-TrackerToken": PIVOTAL_API_TOKEN})
+def _get_json(url):
+
+    print "getting url", url
+
+    res = requests.get(url, headers={"X-TrackerToken": PIVOTAL_API_TOKEN})
+
     if res.status_code != 200:
         raise Exception('error with pivotal tracker: %s' % res.content)
 
     return res.json()
+
+
+def _get_stories(project_id, label):
+    url = "https://www.pivotaltracker.com" \
+          "/services/v5/projects/%s/stories" \
+          "?with_label=%s" % (project_id, label)
+    return _get_json(url)
+
+
+def _get_story(project_id, story_id):
+    url = "https://www.pivotaltracker.com" \
+          "/services/v5/projects/%s/stories/%s" \
+          % (project_id, story_id)
+    return _get_json(url)
+
+
+def _get_tasks(project_id, story_id):
+    url = "https://www.pivotaltracker.com" \
+          "/services/v5/projects/%s/stories/%s/tasks" \
+          % (project_id, story_id)
+    return _get_json(url)
 
 
 def stories(request, project_id, tag_filter, hash_key):
@@ -35,3 +58,12 @@ def stories(request, project_id, tag_filter, hash_key):
         'stories': _get_stories(project_id, tag_filter),
     }
     return render(request, 'pivotal_stories.html', context)
+
+
+def tasks(request, project_id, tag_filter, hash_key, story_id):
+    _verify_hash(project_id, tag_filter, hash_key)
+    context = {
+        'story': _get_story(project_id, story_id),
+        'tasks': _get_tasks(project_id, story_id),
+    }
+    return render(request, 'pivotal_tasks.html', context)
